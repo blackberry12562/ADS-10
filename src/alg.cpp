9348901d0d87e6fc9931d07f432e799c80de8e73
blackberry12562 <116704068+blackberry12562@users.noreply.github.com>
2023-05-31 19:31:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,12 +4,14 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "tree.h"
+
 uint64_t fact(uint16_t n) {
     uint64_t result = 1;
     for (uint64_t i = 1; i <= n; i++)
         result *= i;
     return result;
 }
+
 std::vector<char> getPerm(const Tree& tree, int n) {
     if (tree.getSize() == 0 || n > fact(tree.getSize()))
         return {};
@@ -18,7 +20,7 @@ std::vector<char> getPerm(const Tree& tree, int n) {
     std::vector<char> result;
     for (int i = 0; i < tree.getSize(); i++) {
         int temp = fact(ptr->getSize()-1);
-        ptr = ptr->getChild(x);
+        ptr = ptr->getChild(n / temp);
         result.push_back(ptr->getValue());
         n%=temp;
     }
